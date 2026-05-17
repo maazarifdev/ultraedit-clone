d6b5e418db772114f14a6282427b1d034671671c
user.name"maazarifdev <maaz.arif.codes@gmail.com>
2026-05-17 19:36:17 +0500

    Final changes UltraEdit Update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
-  var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
 
-    loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-=======
-  var swiper = new Swiper(".mySwiper", {
-
-    loop: true,
-
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
->>>>>>> cce9f0c05f22af2534ba2b93ad7312e6f0a44f08
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
