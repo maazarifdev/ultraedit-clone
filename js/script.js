5115f532b8c6d5e06a7efca0925bb76d43be36be
user.name"maazarifdev <maaz.arif.codes@gmail.com>
2026-05-17 12:56:54 +0500

    Initial commit - UltraEdit clone

---
+++ js/script.js
@@ -0,0 +1,15 @@
+  var swiper = new Swiper(".mySwiper", {
+
+    loop: true,
+
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
